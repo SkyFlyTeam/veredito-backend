82ff57763bddd7f7035f9cc57037a3ea483f6ea3
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2026-03-20 13:34:47 -0300

    VER-3 feat: adicionar upload de arquivos

--- src/peticao/controller/peticao.controller.ts
+++ src/peticao/controller/peticao.controller.ts
@@ -1,16 +1,66 @@
-import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { BadRequestException, Controller, Get, HttpCode, Param, ParseIntPipe, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { PeticaoService } from '../service/peticao.service';
 import { PeticaoResponseDTO } from '../dto/peticao-response.dto';
 import { JwtAuthGuard } from '../../account/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../../account/auth/guards/roles.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { existsSync, mkdirSync } from 'fs';
+import { UploadPeticaoDto } from '../dto/upload-peticao.dto';
 
 @ApiTags('Petições')
 @ApiBearerAuth('access-token')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('peticao')
 export class PeticaoController {
-  constructor(private readonly peticaoService: PeticaoService) {}
+  constructor(private readonly peticaoService: PeticaoService) { }
+
+  @Post('upload')
+  @HttpCode(201)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          const uploadPath = './uploads/peticoes/';
+          if (!existsSync(uploadPath)) {
+            mkdirSync(uploadPath, { recursive: true });
+          }
+          cb(null, uploadPath);
+        },
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const sanitizedOriginalName = file.originalname.replace(/\s+/g, '_');
+          cb(null, `${uniqueSuffix}-${sanitizedOriginalName}`);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(pdf|docx|txt)$/)) {
+          return cb(
+            new BadRequestException(
+              'Apenas arquivos .pdf, .docx e .txt são permitidos',
+            ),
+            false,
+          );
+        }
+        cb(null, true);
+      },
+      limits: {
+        fileSize: 300 * 1024 * 1024,
+      },
+    }),
+  )
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: UploadPeticaoDto })
+  @ApiOperation({ summary: 'Fazer upload de uma nova petição' })
+  @ApiResponse({ status: 201, description: 'Petição criada com sucesso' })
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Arquivo é obrigatório');
+    }
+
+    return this.peticaoService.create(file.path, 1);
+  }
 
   @Get()
   @ApiOperation({ summary: 'Listar todas as petições' })
@@ -30,8 +80,13 @@ export class PeticaoController {
     description: 'Petição retornada com sucesso',
     type: PeticaoResponseDTO,
   })
-  @ApiResponse({ status: 404, description: 'Petição não encontrada' })
-  findOne(@Param('id', ParseIntPipe) id: number): Promise<PeticaoResponseDTO> {
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<PeticaoResponseDTO> {
     return this.peticaoService.findOne(id);
   }
 }
+function diskStorage(arg0: { destination: (req: any, file: any, cb: any) => void; filename: (req: any, file: any, cb: any) => void; }): any {
+  throw new Error('Function not implemented.');
+}
+
