3c8ca17a5437f3e8d4e198bb8860547be058181e
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2026-04-21 19:12:59 -0300

    VER-59 feat: Implementando estrutura de envio para o frontend, implementação do controller novo, com rota de get

--- src/peticao/peticao.module.ts
+++ src/peticao/peticao.module.ts
@@ -4,6 +4,7 @@ import { CommandRunnerModule } from 'nest-commander';
 import { PeticaoEntity } from './entity/peticao.entity';
 import { PeticaoService } from './service/peticao.service';
 import { PeticaoController } from './controller/peticao.controller';
+import { PeticaoStreamController } from './controller/peticao-stream.controller';
 import { WordProcessingService } from './pipeline-services/word_processing/word-processing.service';
 import { TextProcessingService } from './pipeline-services/word_processing/text-processing.service';
 import { PipelineOrchestrator } from './pipeline-services/pipeline_orchestror';
@@ -23,7 +24,7 @@ import { SynthesisModule } from '../synthesis/synthesis.module';
     PrecedenteModule,
     SynthesisModule,
   ],
-  controllers: [PeticaoController],
+  controllers: [PeticaoController, PeticaoStreamController],
   providers: [
     PeticaoService,
     WordProcessingService,
