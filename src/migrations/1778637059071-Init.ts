1a2d8280c93d2f6a627ece9bdfd9397d755928f0
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2026-05-13 13:49:16 -0300

    VER-81 fix: ajustando migration gerada, para focar apenas no campo adicionado, e manter os padrões de codigo limpo e organizado.

--- src/migrations/1778637059071-Init.ts
+++ src/migrations/1778637059071-Init.ts
@@ -4,45 +4,11 @@ export class Init1778637059071 implements MigrationInterface {
     name = 'Init1778637059071'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."idx_questao"`);
-        await queryRunner.query(`DROP INDEX "public"."idx_tese"`);
-        await queryRunner.query(`DROP INDEX "public"."idx_peticao_questao"`);
-        await queryRunner.query(`DROP INDEX "public"."idx_peticao_tese"`);
         await queryRunner.query(`ALTER TABLE "precedente_sugerido" ADD "created_at" TIMESTAMP NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "precedente" DROP COLUMN "tese_vetor"`);
-        await queryRunner.query(`ALTER TABLE "precedente" ADD "tese_vetor" vector`);
-        await queryRunner.query(`ALTER TABLE "precedente" DROP COLUMN "questao_vetor"`);
-        await queryRunner.query(`ALTER TABLE "precedente" ADD "questao_vetor" vector`);
-        await queryRunner.query(`ALTER TABLE "precedente_sugerido" ALTER COLUMN "sintese_explicativa" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "resumo"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "resumo" text`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "tese_vetor"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "tese_vetor" vector`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "questao_vetor"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "questao_vetor" vector`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "sobrenome" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "senha" DROP DEFAULT`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "senha" SET DEFAULT '\\x'`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "sobrenome" SET DEFAULT ''`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "questao_vetor"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "questao_vetor" vector(1536)`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "tese_vetor"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "tese_vetor" vector(1536)`);
-        await queryRunner.query(`ALTER TABLE "peticao" DROP COLUMN "resumo"`);
-        await queryRunner.query(`ALTER TABLE "peticao" ADD "resumo" character varying`);
-        await queryRunner.query(`ALTER TABLE "precedente_sugerido" ALTER COLUMN "sintese_explicativa" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "precedente" DROP COLUMN "questao_vetor"`);
-        await queryRunner.query(`ALTER TABLE "precedente" ADD "questao_vetor" vector(1536)`);
-        await queryRunner.query(`ALTER TABLE "precedente" DROP COLUMN "tese_vetor"`);
-        await queryRunner.query(`ALTER TABLE "precedente" ADD "tese_vetor" vector(1536)`);
         await queryRunner.query(`ALTER TABLE "precedente_sugerido" DROP COLUMN "created_at"`);
-        await queryRunner.query(`CREATE INDEX "idx_peticao_tese" ON "peticao" ("tese_vetor") `);
-        await queryRunner.query(`CREATE INDEX "idx_peticao_questao" ON "peticao" ("questao_vetor") `);
-        await queryRunner.query(`CREATE INDEX "idx_tese" ON "precedente" ("tese_vetor") `);
-        await queryRunner.query(`CREATE INDEX "idx_questao" ON "precedente" ("questao_vetor") `);
     }
 
 }
