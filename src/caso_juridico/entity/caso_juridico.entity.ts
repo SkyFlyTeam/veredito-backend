ceaac653c1053bb7a0d0b2e0636bd8bec24e20e3
Brenno <brennolyrio@gmail.com>
2026-05-13 18:08:34 -0300

    VER-87: criação da tabela 'caso_juridico'

---
+++ src/caso_juridico/entity/caso_juridico.entity.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { UserEntity } from '../../account/user/entity/user.entity';
+import { TribunalPrecedenteEntity } from '../../precedents/entity/tribunal_precedente.entity';
+import { CasoPrecedenteSugeridoEntity } from './caso_precedente_sugerido.entity';
+import EntityInterface from 'src/interfaces/entity.interface';
+
+@Entity('caso_juridico')
+export class CasoJuridicoEntity implements EntityInterface {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  area_direito: string;
+
+  @Column({ type: 'text' })
+  pedidos_principais: string;
+
+  @Column({ type: 'text' })
+  tese_pretendida: string;
+
+  @Column({ type: 'char', length: 2 })
+  uf: string;
+
+  @Column({ type: 'text', nullable: true })
+  fundamentos_juridicos: string;
+
+  @Column({ type: 'text', nullable: true })
+  fatos_estruturados: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({ name: 'usuario_id' })
+  usuarioId: number;
+
+  @Column({ name: 'tribunal_precedente_id', nullable: true })
+  tribunalPrecedenteId: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.casosJuridicos)
+  @JoinColumn({ name: 'usuario_id' })
+  usuario: UserEntity;
+
+  @ManyToOne(() => TribunalPrecedenteEntity, { nullable: true })
+  @JoinColumn({ name: 'tribunal_precedente_id' })
+  tribunal_precedente?: TribunalPrecedenteEntity;
+
+  @Exclude()
+  @OneToMany(
+    () => CasoPrecedenteSugeridoEntity,
+    (casoPrecedente: CasoPrecedenteSugeridoEntity) => casoPrecedente.casoJuridico,
+  )
+  precedentesSugeridos: CasoPrecedenteSugeridoEntity[];
+}
