59fd695aa72a9d1bb4652c8c3e9f61f8f4fa8844
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2026-05-21 18:52:34 -0300

    VER-92 feat: adicionar serviço de geração de secoes de petição

--- src/caso_juridico/caso_juridico.module.ts
+++ src/caso_juridico/caso_juridico.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { CasoJuridicoEntity } from './entity/caso_juridico.entity';
 import { CasoPrecedenteSugeridoEntity } from './entity/caso_precedente_sugerido.entity';
 import { SecoesPeticaoEntity } from './entity/secoes_peticao.entity';
+import { CasoJuridicoService } from './service/caso-juridico.service';
+import { CasoJuridicoController } from './controller/caso-juridico.controller';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([
       CasoJuridicoEntity,
       CasoPrecedenteSugeridoEntity,
       SecoesPeticaoEntity,
     ]),
   ],
-  providers: [],
-  controllers: [],
-  exports: [TypeOrmModule],
+  providers: [CasoJuridicoService],
+  controllers: [CasoJuridicoController],
+  exports: [TypeOrmModule, CasoJuridicoService],
 })
 export class CasoJuridicoModule { }
