72164c5bebeb841dc82c74a6a157804c9c9694d3
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2026-05-26 19:44:18 -0300

    VER-108 feat: adicionar serviço de minuta de sentença

--- src/processo/processo.module.ts
+++ src/processo/processo.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ProcessoController } from './controller/processo.controller';
 import { TextSearchPartsService } from './service/text-search-parts.service';
 import { ProcessoService } from './service/processo.service';
 import { ProcessInformationService } from './service/process-information.service';
 import { WordProcessingService } from 'src/peticao/pipeline-services/word_processing/word-processing.service';
+import { MinutaSentencaService } from './service/minuta-sentenca.service';
 import ProcessoJuridicoEntity from './entity/processo_juridico.entity';
 import TipoPecaEntity from './entity/tipo_peca.entity';
 import PecaEntity from './entity/peca.entity';
@@ -19,9 +21,11 @@ import { TipoPecaSeed } from './seed/tipo-peca.seed';
       TipoPecaEntity,
       PecaEntity,
     ]),
+    ConfigModule,
   ],
   controllers: [ProcessoController],
-  providers: [TextSearchPartsService, ProcessoService, ProcessInformationService, WordProcessingService, TipoPecaSeed],
-  exports: [TextSearchPartsService, ProcessoService, ProcessInformationService],
+  providers: [TextSearchPartsService, ProcessoService, ProcessInformationService, WordProcessingService, TipoPecaSeed, MinutaSentencaService],
+  exports: [TextSearchPartsService, ProcessoService, ProcessInformationService, MinutaSentencaService],
 })
 export class ProcessoModule { }
+
