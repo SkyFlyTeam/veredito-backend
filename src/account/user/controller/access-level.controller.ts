0be47be8316e8c265372115756a16549f7798694
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2026-05-31 20:46:30 -0300

    fix: ajustando proteção de rota para que o select de cargo funcione corretamente no frontend

--- src/account/user/controller/access-level.controller.ts
+++ src/account/user/controller/access-level.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/account/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/account/auth/guards/roles.guard';
+import { Public } from 'src/account/auth/decorators/public.decorator';
 import { AccessLevelService } from '../service/access-level.service';
 import { AccessLevelEntity } from '../entity/access-level.entity';
 
@@ -13,6 +14,7 @@ export class AccessLevelController {
   constructor(private readonly accessLevelService: AccessLevelService) {}
 
   @Get()
+  @Public()
   @ApiOperation({ summary: 'Listar todos os níveis de acesso (exceto superuser)' })
   @ApiResponse({
     status: 200,
